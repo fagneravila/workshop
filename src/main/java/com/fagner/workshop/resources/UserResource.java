4cb417d8a93a295b623d2e2f153468df648d5c0d
Fagner Avila <fagner.avila@gmail.com>
2022-08-26 23:40:32 -0300

    padrao DTO

--- src/main/java/com/fagner/workshop/resources/UserResource.java
+++ src/main/java/com/fagner/workshop/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.fagner.workshop.resources;
 
 import com.fagner.workshop.domain.User;
+import com.fagner.workshop.dto.UserDTO;
 import com.fagner.workshop.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -23,14 +25,18 @@ public class UserResource {
 
     //@RequestMapping(method = RequestMethod.GET)
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
-       /* User fagner = new User("1", "Fagner", "fagner.avila@gmail");
-        User avila = new User("2", "agner", "agner.avila@gmail");
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(fagner,avila)); */
+    public ResponseEntity<List<UserDTO>> findAll(){
+
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO );
     }
 
 
+    /* public ResponseEntity<List<User>> findAll(){
+       User fagner = new User("1", "Fagner", "fagner.avila@gmail");
+        User avila = new User("2", "agner", "agner.avila@gmail");
+        List<User> list = new ArrayList<>();
+        list.addAll(Arrays.asList(fagner,avila));
+        return ResponseEntity.ok().body(list);*/
 }
