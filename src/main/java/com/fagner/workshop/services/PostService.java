1f2862b3ce66e255d7c2e30c7b76bdfeeb9a0319
Fagner Avila <fagner.avila@gmail.com>
2022-09-02 00:58:08 -0300

    post por ido

---
+++ src/main/java/com/fagner/workshop/services/PostService.java
@@ -0,0 +1,31 @@
+package com.fagner.workshop.services;
+
+import com.fagner.workshop.domain.Post;
+import com.fagner.workshop.domain.User;
+import com.fagner.workshop.repository.PostRepository;
+import com.fagner.workshop.services.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService
+{
+    @Autowired
+    private PostRepository repository;
+
+
+    public List<Post> findAll(){
+        return repository.findAll();
+    }
+
+    public Post findById(String id) {
+        Optional<Post> post = repository.findById(id);
+        return post.orElseThrow(() -> new ResourceNotFoundException("Post não encontrado"));
+    }
+
+
+
+}
