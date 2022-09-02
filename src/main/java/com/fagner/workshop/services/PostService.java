a1f7aff7eccca19313ba2653d5cb2445ecd52fc5
Fagner Avila <fagner.avila@gmail.com>
2022-09-02 02:39:10 -0300

    consulta com varios criterios

--- src/main/java/com/fagner/workshop/services/PostService.java
+++ src/main/java/com/fagner/workshop/services/PostService.java
@@ -7,6 +7,7 @@ import com.fagner.workshop.services.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,6 +32,12 @@ public class PostService
      //  return repository.findByTitleContainingIgnoreCase(text);
 
        return repository.searchTitle(text);
+    }
+
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+            maxDate = new Date(maxDate.getTime() + 24 * 60 *60 *1000);
+            return repository.fullSearch(text, minDate, maxDate);
     }
 
 
